0e4daedfbfc6c9dd82243cf252cffb1cf70d714d
Josue Molina <eusojkeivn@gmail.com>
2017-11-13 23:53:56 -0600

    Refactorizando control de los links LinksController.js

--- WebScraping/WebScraping/app/controllers/LinksController.js
+++ WebScraping/WebScraping/app/controllers/LinksController.js
@@ -3,94 +3,92 @@
   $scope.loading = true;
   var isReady = false;
 
-  if (!isReady) {
-    LinksService.getLinksWeb().then(
-      function (response) {
-        $scope.lists = response.Lists;
+  if (!isReady) 
+  {
+    LinksService.getLinksWeb().then(function (response) 
+    {
+      loadLinks(response);
+    }, 
+    function () 
+    { 
+        console.log('It wasn\'t possible to load the data');
+    });
+  }
 
-        helper.addClasses();
-        helper.rating();
+  function loadLinks(response) {
 
-        var jsonString = JSON.stringify($scope.lists);
-        jsonString = helper.replaceAll(jsonString, "#text", "text");
-        jsonString = helper.replaceAll(jsonString, "@class", "class");
-        jsonString = helper.replaceAll(jsonString, "@name", "name");
-        jsonString = helper.replaceAll(jsonString, "@href", "href");
+    helper.addClasses();
+    helper.rating();
 
-        var localLists = JSON.parse(jsonString);
+    var tempLists = buildJsonList(response.Lists);
 
-        localLists.List.forEach(function(data) {
+    //insert fewer links using this method (519~518)
+    //tempLists = removeBrokenLinks(tempLists);
 
-          data.ul.li.forEach(function(link) {
+    //Not very effecient, but insert more links 
+    //duplicating this block, don't know why (548)
+    tempLists.List.forEach(function(data) {
+      data.ul.li.forEach(function(link) {
+        if ( link.a.text == undefined
+          || link.a.href == undefined
+          || link.a.constructor === Array )
+        {
+          data.ul.li.splice(data.ul.li.indexOf(link), 1);
+        }
+      });
+    });
 
-            if (link.a.text == undefined || link.a.href == undefined || link.a.constructor === Array)
-            {
-              data.ul.li.splice(data.ul.li.indexOf(link), 1);
-            }
+    tempLists.List.forEach(function (data) {
+      data.ul.li.forEach(function (link) {
+        if ( link.a.text == undefined
+          || link.a.href == undefined
+          || link.a.constructor === Array )
+        {
+          data.ul.li.splice(data.ul.li.indexOf(link, 1));
+        }
+      });
+    });
 
-          });
+    $scope.lists = tempLists;
+    $scope.loading = false;
 
-        });
+    insertNewData($scope.lists);
 
-        //Not very effecient
-        localLists.List.forEach(function (data) {
-          data.ul.li.forEach(function (link) {
-            if (link.a.text == undefined || link.a.href == undefined || link.a.constructor === Array) {
-              data.ul.li.splice(data.ul.li.indexOf(link, 1));
-            }
-          });
-        });
+    addEventListeners();
 
-        $scope.lists = localLists;
-        $scope.loading = false;
+  };
 
-        $scope.lists.List.forEach(function (data) {
+  function insertNewData(lists){
 
-          category = {};
-          category.CategoryName = data.h2.text;
-          LinksService.addCategory(category).then(function(response) {
+    lists.List.forEach(function (data) {
 
-            category.Id = response.id;
+      category = {};
+      category.CategoryName = data.h2.text;
 
-            data.ul.li.forEach(function (link) {
+      LinksService.addCategory(category).then( function(response)
+      {
+        addingLinks(response, data);
+      }, 
+      function () 
+      { 
+        console.log('An error ocurred while storing/rendering the data');
+      });
 
-              if (link.text != undefined) {
+      isReady = true;
 
-                link.text[0] = helper.replaceAll(link.text[0], "[", "");
-                link.text[0] = helper.replaceAll(link.text[0], "]", "");
-                link.text[0] = helper.replaceAll(link.text[0], "(", "");
-                link.text[0] = helper.replaceAll(link.text[0], ")", "");
+    });
 
-                link.text = link.text[0];
-              }
+  }
 
-              newLink = {};
-              newLink.Name = link.a.text;
-              newLink.LinkValue = link.a.href;
-              newLink.NumberOfRatings = 0;
-              newLink.Rating = 0;
-              newLink.IdCategory = category.Id;
+  function addingLinks(response, data){
+    
+    category.Id = response.id;
+    
+    data.ul.li.forEach(function (link) {
+      newLink = buildLink(link, category.Id);
+      LinksService.addLink(newLink);
+    });
 
-              LinksService.addLink(newLink).then(function () { }, function () { });
-            });
-
-
-          }, function () { });
-
-          isReady = true;
-
-        });
-
-        setTimeout(function () {
-          $("input[type='range']").on("input change", function () {
-            $(this).parent(".col-sm-6").siblings(".col-sm-5").find("span")
-              .html($(this).val() + "<small style='font-size:.5em;'> pts.</small>");
-          });
-
-        }, 10);
-      },
-      function () { }
-    );
   }
 
   $scope.enviarPuntuacion = function (e) {
@@ -106,4 +104,66 @@
       return "even";
     return "odd";
   }
-});
+
+}); //End of angular controller block
+
+function buildJsonList(lists){
+
+  var jsonString = JSON.stringify(lists);
+  jsonString = helper.replaceAll(jsonString, "#text", "text");
+  jsonString = helper.replaceAll(jsonString, "@class", "class");
+  jsonString = helper.replaceAll(jsonString, "@name", "name");
+  jsonString = helper.replaceAll(jsonString, "@href", "href");
+
+  return JSON.parse(jsonString);
+
+}
+
+//insert fewer links using this method (519~518) 
+function removeBrokenLinks(jsonLists) {
+  
+  jsonLists.List.forEach(function (data) {
+    data.ul.li.forEach(function (link) {
+      if ( link.a.text == undefined
+        || link.a.href == undefined
+        || link.a.constructor === Array )
+      {
+        data.ul.li.splice(data.ul.li.indexOf(link, 1));
+      }
+    });
+  });
+
+  return jsonLists;
+
+}
+
+function buildLink(link, categoryId){
+  
+  if (link.text != undefined) {
+    
+    link.text[0] = helper.replaceAll(link.text[0], "[", "");
+    link.text[0] = helper.replaceAll(link.text[0], "]", "");
+    link.text[0] = helper.replaceAll(link.text[0], "(", "");
+    link.text[0] = helper.replaceAll(link.text[0], ")", "");
+
+    link.text = link.text[0];
+  }
+
+  newLink = {};
+  newLink.Name = link.a.text;
+  newLink.LinkValue = link.a.href;
+  newLink.NumberOfRatings = 0;
+  newLink.Rating = 0;
+  newLink.IdCategory = categoryId;
+
+  return newLink;
+}
+
+function addEventListeners(){
+  setTimeout( function () {
+    $("input[type='range']").on("input change", function () {
+      $(this).parent(".col-sm-6").siblings(".col-sm-5").find("span")
+        .html($(this).val() + "<small style='font-size:.5em;'> pts.</small>");
+    });
+  }, 10);
+}
